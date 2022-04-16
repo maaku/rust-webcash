f4ed6eb143494ba82a6d80f6874e558884004836
Mark Friedenbach <mark@friedenbach.org>
2022-04-16 11:38:31 -0700

    Implement stub API endpoints.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,28 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello, world!")
+}
+
+#[get("/api/v1/replace")]
+async fn replace() -> impl Responder {
+    HttpResponse::Ok().body("replace")
+}
+
+#[get("/api/v1/target")]
+async fn target() -> impl Responder {
+    HttpResponse::Ok().body("target")
+}
+
+#[get("/api/v1/mining_report")]
+async fn mining_report() -> impl Responder {
+    HttpResponse::Ok().body("mining_report")
+}
+
+#[get("/api/v1/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("health_check")
 }
 
 #[actix_web::main]
@@ -17,7 +37,11 @@ async fn main() -> std::io::Result<()> {
     println!("Quit the server with CONTROL-C.");
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(index)
+            .service(replace)
+            .service(target)
+            .service(mining_report)
+            .service(health_check)
     })
     .bind(("127.0.0.1", 8000))?
     .run()
