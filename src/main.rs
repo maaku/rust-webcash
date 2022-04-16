fe2ff14875733c8fdc17d4ddffc2713db0f14140
Mark Friedenbach <mark@friedenbach.org>
2022-04-16 11:37:54 -0700

    Add actix-web server boiler plate.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,24 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server instance at http://127.0.0.1:8000/");
+    println!("Quit the server with CONTROL-C.");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
 
 // End of File
